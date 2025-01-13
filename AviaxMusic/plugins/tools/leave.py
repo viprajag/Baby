4d81cb88001241d251d700d2a638e624f637e2de
NKMODNK7 <botbabbu68@gmail.com>
2025-01-13 10:22:08 +0530

    Update leave.py

--- AviaxMusic/plugins/tools/leave.py
+++ AviaxMusic/plugins/tools/leave.py
@@ -3,9 +3,9 @@ from pyrogram.types import Message
 from pyrogram.enums import ChatType
 
 from config import LOG_GROUP_ID
-from AviaxMusic import app, userbot
-from AviaxMusic.misc import SUDOERS
-from AviaxMusic.utils.database import get_client
+from AnonXMusic import app, userbot
+from AnonXMusic.misc import SUDOERS
+from AnonXMusic.utils.database import get_client
 
 
 @app.on_message(filters.command(["leaveall1", "leaveall2", "leaveall3", "leaveall4", "leaveall5"]) & SUDOERS)
