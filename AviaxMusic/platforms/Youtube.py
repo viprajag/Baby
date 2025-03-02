d8c5b0bd8e303fe6db0e6328b8303beb65682145
NKMODNK11 <j19502570@gmail.com>
2025-03-02 08:10:02 +0530

    Update Youtube.py

--- AviaxMusic/platforms/Youtube.py
+++ AviaxMusic/platforms/Youtube.py
@@ -9,9 +9,9 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch, CustomSearch
 
-from AnonXMusic.utils.database import is_on_off
-from AnonXMusic.utils.formatters import time_to_seconds
-from AnonXMusic import LOGGER
+from AviaxMusic.utils.database import is_on_off
+from AviaxMusic.utils.formatters import time_to_seconds
+from AviaxMusic import LOGGER
 
 
 import os
