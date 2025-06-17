50cd71b297362a2c5e0ea30ee1c9cb75e3b98233
SAHIL SURYAVANSHI <sahilsingh56701@gmail.com>
2025-06-17 12:58:21 +0530

    Update call.py

--- AviaxMusic/core/call.py
+++ AviaxMusic/core/call.py
@@ -35,7 +35,7 @@ from AviaxMusic.utils.exceptions import AssistantErr
 from AviaxMusic.utils.formatters import check_duration, seconds_to_min, speed_converter
 from AviaxMusic.utils.inline.play import stream_markup
 from AviaxMusic.utils.stream.autoclear import auto_clean
-from AviaxMusic.utils.thumbnails import gen_thumb
+from AviaxMusic.utils.thumbnails import get_thumb
 from strings import get_string
 
 autoend = {}
