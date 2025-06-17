88fcf34cdf2e92760532c9c3fc35dd42d17f11b2
SAHIL SURYAVANSHI <sahilsingh56701@gmail.com>
2025-06-17 13:11:53 +0530

    Update skip.py

--- AviaxMusic/plugins/admins/skip.py
+++ AviaxMusic/plugins/admins/skip.py
@@ -9,7 +9,7 @@ from AviaxMusic.utils.database import get_loop
 from AviaxMusic.utils.decorators import AdminRightsCheck
 from AviaxMusic.utils.inline import close_markup, stream_markup
 from AviaxMusic.utils.stream.autoclear import auto_clean
-from AviaxMusic.utils.thumbnails import gen_thumb
+from AviaxMusic.utils.thumbnails import get_thumb
 from config import BANNED_USERS
 
 
