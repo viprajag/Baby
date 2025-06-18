0f8f5ba83102bf7b1682fc6c479b201268ae1dfd
NKMODNK11 <j19502570@gmail.com>
2025-06-18 11:33:46 +0530

    Update leaveall.py

--- AviaxMusic/plugins/sudo/leaveall.py
+++ AviaxMusic/plugins/sudo/leaveall.py
@@ -6,7 +6,7 @@ from AviaxMusic import app, userbot
 from AviaxMusic.misc import SUDOERS
 from AviaxMusic.utils.database import get_assistant, is_active_chat, get_client
 from AviaxMusic.core.userbot import assistants
-from config import LOGGER_ID as JAI
+from config import LOG_GROUP_ID as JAI
 
 
 @app.on_message(filters.command(["leaveall1", f"leaveall1@{app.username}"]) & SUDOERS)
