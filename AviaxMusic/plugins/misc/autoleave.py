851a6c7e735cf481c801dba91e1c44520f1c771e
NKMODNK11 <j19502570@gmail.com>
2025-06-19 12:09:33 +0530

    Update autoleave.py

--- AviaxMusic/plugins/misc/autoleave.py
+++ AviaxMusic/plugins/misc/autoleave.py
@@ -10,7 +10,7 @@ from AviaxMusic.utils.database import get_client, set_loop, is_active_chat, is_a
 import logging
 
 async def auto_leave():
-    while not await asyncio.sleep(28080):
+    while not await asyncio.sleep(86400):
         from AviaxMusic.core.userbot import assistants
         ender = await is_autoleave()
         if not ender:
